4498af2554f415f9f0e7accd73e9241b6f5f37cd
Alex <alex@scotttonge.com>
2018-01-16 15:54:50 +0000

    raises error for negative values and values higher than 250

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,8 +2,11 @@ require 'matrix'
 
 class Bitmap
 
-  def initialize(n, m)
-    @matrix = Matrix.build(m, n) { 'O' }
+  def build_grid(n, m)
+    if n.between?(1, 250) && m.between?(1, 250)
+      @matrix = Matrix.build(m, n) { 'O' }
+    else raise IntegerValueError, 'Number must be between 1-250'
+    end
   end
 
   def create
