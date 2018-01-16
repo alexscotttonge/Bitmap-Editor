a7ee0ef54fccf896beabc2355fab11c2d4cf9657
Alex <alex@scotttonge.com>
2018-01-16 15:57:08 +0000

    refactored tests

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -2,21 +2,20 @@ describe Bitmap do
 
   IntegerValueError = Class.new(StandardError)
 
+  subject(:bitmap) { Bitmap.new }
+
   describe "#build_grid" do
     it 'raises an error if N or M dimension is higher than 250' do
-      bitmap = Bitmap.new
       expect { bitmap.build_grid(251, 251) }.to raise_error(IntegerValueError)
     end
 
     it "raises an error for negative values" do
-      bitmap = Bitmap.new
       expect { bitmap.build_grid(-1, -3) }.to raise_error(IntegerValueError)
     end
   end
 
   describe "#create" do
     it 'starts with a grid of M x N' do
-      bitmap = Bitmap.new
       bitmap.build_grid(2, 2)
       expect { bitmap.create }.to output("OO\nOO\n").to_stdout
     end
