8aaa33038c5d098685e7b9b7062690407a3490d2
Alex <alex@scotttonge.com>
2018-01-16 15:10:17 +0000

    outputs grid of M x N

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,4 +2,17 @@ require 'matrix'
 
 class Bitmap
 
+  def initialize(n, m)
+    @matrix = Matrix.build(m, n) { 'O' }
+  end
+
+  def create
+    str = ""
+    @matrix.to_a.each do |row|
+      str += row.join("")
+      str += "\n"
+    end
+    puts str
+  end
+
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,4 +1,10 @@
 describe Bitmap do
 
+  describe "#create" do
+    it 'starts with a grid of M x N' do
+      bitmap = Bitmap.new(2,2)
+      expect { bitmap.create }.to output("OO\nOO\n").to_stdout
+    end
+  end
 
 end
